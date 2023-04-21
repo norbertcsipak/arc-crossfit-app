4d6c383a3088405392c3b7470168423da83a039a
angelee otic <otic.angeleemae@gmail.com>
2023-04-21 20:53:02 +0100

    seeds

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,5 +4,5 @@ class Member < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :dashboard
-  has_many :workouts
+  has_many :workouts, through: :dashboard
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Workout.delete_all
+Member.delete_all
+Dashboard.delete_all
+
+puts "creating members..."
+
+member1 = Member.new(
+  id: 1,
+  email: "ange@test.com",
+  first_name: "Angee",
+  last_name: "Otic",
+  BIO: "Hellow, I enjoy working out",
+  DOB: "29-05-1995",
+  password: "seeded1"
+)
+
+member1.save
+
+puts "created #{member1}!!"
+
+workout1 = Workout.new(
+  id: 1,
+  name: "angee",
+  description: "as many reps a possible"
+)
+
+workout1.save
+
+puts "created #{workout1}!!"
+
+dash1 = Dashboard.new(
+  id: 1,
+  score: "nil"
+)
+
+dash1.save!
+
+# dash1.create(workout1)
+
+puts "created #{dash1}!!"
